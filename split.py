2a893571e0ba9437a3a984d8941530cd253980c3
Danya Alexeyevsky <daniil.alexeyevsky@hipasus.com>
2024-04-20 16:13:05 +0300

    Use lib in split.

--- split.py
+++ split.py
@@ -1,34 +1,18 @@
 #!/usr/bin/env python3
-import xml.etree.ElementTree as ET
 import argparse
 from pathlib import Path
 
-ns = dict(gpx="http://www.topografix.com/GPX/1/1")
+from lib import GPX
+from lib import Track
 
-args = argparse.ArgumentParser()
-args.add_argument('tracks', nargs='*')
-args.add_argument('--format', default='{filename}_{segment:03d}.gpx')
-args = args.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tracks", nargs="*")
+    parser.add_argument("--format", default="{filename}_{segment:03d}.gpx")
+    args = parser.parse_args()
 
-gpx_template_root = ET.fromstring("""
-<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-<gpx
-	xmlns="http://www.topografix.com/GPX/1/1"
-	creator="http://nakarte.me"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"
-	version="1.1">
-	<trk><trkseg></trkseg></trk>
-</gpx>
-""".strip())
-gpx_track = gpx_template_root.find('.//gpx:trk', ns)
-gpx_template = ET.ElementTree(gpx_template_root)
-ET.register_namespace('', ns['gpx'])
-
-for filename in args.tracks:
-	root = ET.parse(filename).getroot()
-	for n, segment in enumerate(root.findall('.//gpx:trkseg', ns), start=1):
-		gpx_track.clear()
-		gpx_track.append(segment)
-		out_filename = args.format.format(filename=Path(filename).stem, segment=n)
-		gpx_template.write(out_filename, method='xml', xml_declaration=True, encoding='utf-8')
+    for filename in args.tracks:
+        for n, segment in enumerate(GPX.from_xml_path(filename).itersegments()):
+            gpx = GPX(tracks=[Track(segments=[segment])], waypoints=[])
+            out_filename = args.format.format(filename=Path(filename).stem, segment=n)
+            gpx.to_xml_path(out_filename)
